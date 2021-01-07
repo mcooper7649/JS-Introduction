71d73b2fac55d799b989b57519e24e1f909caf72
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-01-07 12:00:14 -0500

    Completed getMilk Challenge

--- getMilk.js
+++ getMilk.js
@@ -6,7 +6,7 @@
 // return Money modulo bottle price of 1.5
 // assign the function to a variable named change
 
-function getMilk(money) {   
+function getMilk(money, costPerBottle) {   
     console.log("leaveHouse");
     console.log("moveRight");
     console.log("moveRight");
@@ -16,8 +16,7 @@ function getMilk(money) {
     console.log("moveUp");
     console.log("moveRight");
     console.log("moveRight");
-    var numberOfBottles = Math.floor(money / 1.5);
-    console.log("buy " + numberOfBottles + " bottles of milk");
+    console.log("buy " + calcBottles(money, costPerBottle) + " bottles of milk");
     console.log("moveLeft");
     console.log("moveLeft");
     console.log("moveDown");
@@ -27,7 +26,17 @@ function getMilk(money) {
     console.log("moveLeft");
     console.log("moveLeft");
     console.log("enterHouse");
-    return money % 1.5;  //returns the remainder of the bottles of milk ie; change;
+    return calcChange(money, 1.5);  //returns the remainder of the bottles of milk ie; change;
   }
 
-  var change =  getMilk(5);   
+  function calcBottles(startingMoney, costPerBottle){  // This function takes the input variables money, 1.5 from the getMilk function
+    var numOfBottles = Math.floor(startingMoney / costPerBottle);  // calculates and floors the number of bottles we can buy
+    return numOfBottles  //returns
+  }
+
+  function calcChange(startingAmount, costPerBottle){
+    var change  = startingAmount % costPerBottle;
+    return change;
+  }
+
+  console.log("Hello master, here is your " + getMilk(10, 3) + " change.");
