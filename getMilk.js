fa1343018d9601e0fbb38e1bd0c7632d6384d793
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-01-07 20:00:00 -0600

    Breakdown and Add Notes to JavaScript  Input/Output lesson

--- getMilk.js
+++ getMilk.js
@@ -1,3 +1,11 @@
+//Basic Function with Input and Output
+
+// Declare Function with name and pass Input "Money"
+// Pass Instructions for the function bewteen {}
+// Using Math take money and figure out how many bottles of milk you can buy if the milk is 1.5
+// return Money modulo bottle price of 1.5
+// assign the function to a variable named change
+
 function getMilk(money) {   
     console.log("leaveHouse");
     console.log("moveRight");
@@ -19,6 +27,7 @@ function getMilk(money) {
     console.log("moveLeft");
     console.log("moveLeft");
     console.log("enterHouse");
+    return money % 1.5;  //returns the remainder of the bottles of milk ie; change;
   }
 
-  getMilk(5);
+  var change =  getMilk(5);   
